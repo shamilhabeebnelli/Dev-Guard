39a118272c05e1a1df83defe3156afc197812373
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-06-22 12:37:38 +0530

    𝗣𝗥𝗘𝗠𝗶U𝗠

---
+++ _pyrogram/__main__.py
@@ -0,0 +1,9 @@
+# (C) 2021 Jayant Kageri
+
+from pyrogram import idle, Client, filters
+from _pyrogram import app, LOGGER
+from _pyrogram.modules import *
+import os
+
+print("Pyrogram User Client Started \nPowered By @MwK\n(C) 2021 Shamil Habeeb Nelli")
+app.run()
